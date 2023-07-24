0470471b981a70c2b0ec939ff04a83184584439d
SriPauldaniel <91692450+SriPauldaniel@users.noreply.github.com>
2023-07-24 10:34:32 +0530

    Update HouseRobber.java

--- HouseRobber.java
+++ HouseRobber.java
@@ -11,6 +11,9 @@ public class HouseRobber {
     }
 
     static int findNum(int[] arr,int n) {
+        if(n==1) {
+            return arr[0];
+        }
         int odd = 0,even = 0;
         for(int i=0;i<n;i++) {
             if(i%2 == 0) {
